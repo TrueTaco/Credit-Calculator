2aa19626c9edbbda2c82072ba09ee4f65f663558
True Taco <mc.depcik@gmx.de>
2024-08-14 22:01:28 +0200

    Create Content placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import BackgroundWrapper from "./components/BackgroundWrapper";
+import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
   return (
     <BackgroundWrapper>
       <Header />
+      <Content />
     </BackgroundWrapper>
   );
 }
---
+++ src/components/Content.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface ContentProps {}
+
+const Content: FC<ContentProps> = () => {
+  return <div className="w-full h-full bg-green-500 opacity-30"></div>;
+};
+
+export default Content;
