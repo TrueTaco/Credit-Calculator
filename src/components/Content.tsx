4ba35f9782a47c6f7a644c344a7356401a6a26d2
True Taco <mc.depcik@gmx.de>
2024-11-22 12:09:55 +0100

    Fix ts warnings

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,4 @@
-import { FC, SetStateAction, useState } from "react";
+import { FC, useState } from "react";
 import TabSwitcher from "./TabSwitcher";
 import Calculation from "./View/Calculation/Calculation";
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Header = () => {
   return <div className="bg-red-500 opacity-10 w-full h-16"></div>;
 };
