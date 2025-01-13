4f4b653cd48f2a34ef6ff2f31d466b8513381d78
True Taco <mc.depcik@gmx.de>
2025-01-13 11:51:34 +0100

    Add coloring option for glowWrapper

--- src/components/Generics/GlowWrapper.tsx
+++ src/components/Generics/GlowWrapper.tsx
@@ -3,15 +3,18 @@ import { FC, ReactNode } from "react";
 interface TabSwitcherProps {
   children: ReactNode;
   strength?: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100;
+  color?: string;
 }
 
-const TabSwitcher: FC<TabSwitcherProps> = ({ children, strength = 20 }) => {
-  // Translation
-
+const TabSwitcher: FC<TabSwitcherProps> = ({
+  children,
+  strength = 20,
+  color = "white",
+}) => {
   return (
     <>
       <div
-        className={`absolute inset-0 rounded-full bg-white opacity-${strength} blur-xl`}
+        className={`absolute inset-0 rounded-full bg-${color} opacity-${strength} blur-xl`}
       />
       {children}
     </>
