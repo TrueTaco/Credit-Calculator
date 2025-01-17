02cae681db5b7dcf56792ac45ee0e072b226ab30
True Taco <mc.depcik@gmx.de>
2025-01-17 15:39:06 +0100

    Add placeholder icon for language select

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
-import GlowWrapper from "./Generics/GlowWrapper";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const { i18n } = useTranslation("home");
+
   return (
-    <div className="w-full h-16 items-center flex justify-start pl-4">
+    <div className="w-full h-16 items-center flex justify-between pl-4 pr-4">
       {/* Move into component */}
       <div className="relative">
         <p className="text-[#48D1D9] text-xl font-medium drop-shadow-glow">
@@ -11,7 +13,9 @@ const Header = () => {
       </div>
 
       {/* Move into component */}
-      <div></div>
+      <div className="flex cursor-pointer">
+        <p className="text-white">{i18n.language.toUpperCase()}</p>
+      </div>
     </div>
   );
 };
